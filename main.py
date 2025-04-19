d0f40ee24d42fa772acacf5b7231963338ff333b
Jonathan <jonathan2lozere@gmail.com>
2025-04-19 18:36:09 +0200

    Add server status endpoint for retrieving Xonotic server information

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 from dataclasses import dataclass
-from typing import Union
 import json
 from fastapi import FastAPI, Path
+import socket
+import re
 
 with open("config/server_list.json", "r") as f:
     server_list = json.load(f)
+
+
+def get_xonotic_server_status(host: str, port: int = 26001):
+    request = b'\xFF\xFF\xFF\xFFgetstatus'
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(3)
+
+    try:
+        sock.sendto(request, (host, port))
+        data, _ = sock.recvfrom(4096)
+        response = data.decode('utf-8', errors='ignore')
+
+        max_players_match = re.search(r"sv_maxclients\\(\d+)", response)
+        current_players_match = re.search(r"(?<!sv_)\\clients\\(\d+)", response)
+
+        max_players = int(max_players_match.group(1))
+        current_players = int(current_players_match.group(1))
+
+        available_slots = max_players - current_players
+
+        return {
+            'players': current_players,
+            'max_players': max_players,
+            'available_slots': available_slots
+            }
+
+    except socket.timeout:
+        return {"error": "The server do not responding"}
+    except Exception as e:
+        return {"error": str(e)}
 
 @dataclass
 class Server() :
@@ -24,3 +55,8 @@ def get_all_server() -> list[Server]:
 @app.get("/server/{id}")
 def get_server_by_id(id:int = Path(ge = 0)) -> Server :
     return server_list[id]
+
+@app.get("/server_status/{id}")
+def server_status (id:int = Path(ge = 0)):
+    server =server_list[id]
+    return get_xonotic_server_status("192.168.1.155", server["port"])
